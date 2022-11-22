f7969250d35c3782451379376dc9f9a17b3a4547
andradarmawan <denativo14@gmail.com>
2022-11-22 14:40:07 +0700

    Backend

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express'); //Require memanggil/import library express
+const app = express(); //membuat objek dari class express
+const port = 3000; //membuat variabel port
 const path = require("path");
 const staticpath = path.join(__dirname, "/")
 
-app.use(express.static(staticpath));
+app.use(express.static(staticpath)); //menentukan dimana file server.js dijalankan, memudahkan melacak file HTML
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) { //fungsi untuk merespon request yang diterima
   res.sendFile(staticpath + '/index.html');
 });
 
-app.listen(port);
+app.listen(port); //menjalankan server pada port yang telah ditentukan
